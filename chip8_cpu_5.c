b8cc8277c260e8e7831cd53484981faeecc5ba81
Karim DRIDI <karim.dridi@gmail.com>
2022-08-26 21:00:01 +0200

    generate opcode functions part 1

--- chip8_cpu_5.c
+++ chip8_cpu_5.c
@@ -4,10 +4,7 @@
 
 #include "chip8.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
 // Skip the following instruction if the value of register VX equals the value of register VY
-void chip8_cpu_5XY0() {
+void chip8_cpu_5XY0(){
     SKIP_NEXT_IF(VX == VY);
 }
